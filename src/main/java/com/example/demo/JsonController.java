c7a8fe60fa12696b0fae81fcd2b2120837208f4b
unknown <jjh5025d@gmail.com>
2026-05-14 00:27:45 +0900

    feat: GET /json 추가

---
+++ src/main/java/com/example/demo/JsonController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Map;
+import java.util.LinkedHashMap;
+
+@Controller
+public class JsonController {
+
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> user() {
+
+        Map<String, Object> data = new LinkedHashMap<>();
+
+        data.put("age", 24);
+        data.put("name", "장지훈");
+
+        return data;
+    }
+}
