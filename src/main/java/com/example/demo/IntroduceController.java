eb508e762519b9a5855f9a82ae4887382dec57dd
unknown <jjh5025d@gmail.com>
2026-05-13 23:43:02 +0900

    feat: GET /introduce에 name 쿼리파라미터 추가

--- src/main/java/com/example/demo/IntroduceController.java
+++ src/main/java/com/example/demo/IntroduceController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class IntroduceController {
@@ -10,5 +12,11 @@ public class IntroduceController {
     @GetMapping("/introduce")
     public String introduce() {
         return "introduce";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/introduce", params = "name")
+    public String introduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
     }
 }
