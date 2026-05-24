71462bf66eb54d3ef2418a4142546c9356a8fa2f
unknown <jjh5025d@gmail.com>
2026-05-25 00:23:31 +0900

    feat: View용 DTO 추가

---
+++ src/main/java/com/example/demo/ResponseView.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class ResponseView {
+    private final String boardName;
+    private final String title;
+    private final String authorName;
+    private final String createdTime;
+    private final String content;
+
+    public ResponseView(String boardName, String title, String authorName, String createdTime, String content) {
+        this.boardName = boardName;
+        this.title = title;
+        this.authorName = authorName;
+        this.createdTime = createdTime;
+        this.content = content;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
