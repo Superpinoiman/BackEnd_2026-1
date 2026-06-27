f8484cb97ff694adc3335847971bb0744d480373
unknown <jjh5025d@gmail.com>
2026-06-28 01:55:54 +0900

    feat: Article 도메인에 연관관계 추가

--- src/main/java/com/example/demo/domain/Article.java
+++ src/main/java/com/example/demo/domain/Article.java
@@ -1,11 +1,6 @@
 package com.example.demo.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
@@ -17,11 +12,13 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "board_id")
-    private Long boardId;
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
 
-    @Column(name = "author_id")
-    private Long authorId;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Member member;
 
     private String title;
     private String content;
